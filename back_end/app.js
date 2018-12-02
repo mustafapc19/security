65997c02ee2ce901905d989c9357c95803b1951d
Muhammed <mustafa010199@gmail.com>
2018-12-02 23:01:36 +0530

    DB helper func. added

--- back_end/app.js
+++ back_end/app.js
@@ -1,17 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-var databaseConfig = require('./config/database')
-var Attendance = require('./models/attendance')
+var bodyParser = require('body-parser');
+var databaseConfig = require('./config/database');
+var Attendance = require('./models/attendance');
 
-var pushAttendance = require('./routes/device/pushAttendance')
+var pushAttendance = require('./routes/device/pushAttendance');
 
 mongoose.connect(databaseConfig.address);
 
 /* app.use(express.json())
  */
-app.use(bodyParser())
+app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
@@ -19,23 +19,23 @@ app.use(bodyParser.urlencoded({
 
 
 
-const CronJob = require('cron').CronJob;
+CronJob = require('cron').CronJob;
 console.log('Before job instantiation');
-const job = new CronJob('00 00 00 * * *', function () {
-    const d = new Date();
+job = new CronJob('00 00 00 * * *', function () {
+    d = new Date();
     console.log('Cron :: onTick:', d);
     Attendance.populateEmploys(function (err, doc) {
-        if (err) console.log(err)
-    })
+        if (err) console.log(err);
+    });
 });
 console.log('After job instantiation');
 job.start();
 
 
-app.use('/device/pushAttendance', pushAttendance)
+app.use('/device/pushAttendance', pushAttendance);
 
 
 
 
 
-app.listen(1234)
+app.listen(1234);
