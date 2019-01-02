e0217a16b90ca3db47c0bdba950e40fc5af5d30b
mustafapc19 <mustafa010199@gmail.com>
2019-01-02 19:05:23 +0530

    backbone

--- device/backbone/main.py
+++ device/backbone/main.py
@@ -12,7 +12,36 @@ def pushAttendance():
 
 
 
+
+
+
+def menu(res):
+    term_print("Welcome {res.name}")
+    opts = opts.append("key")
+    index = 0
+    now = time.Now()
+    then = time.Now()
+    while(now-then<4):
+        for(i=0;i<opts.len;i++):
+            if(i==index):
+                term_print(opts[i]).color()
+                continue
+            term_print(opts[i])
+            
+            (flag,inp) = input()
+            if(flag==1)
+        
+
+
+
 def rfidToServer():
+
+
+
+
+
+
+
 
 while(True):
     (flag,inp) = input()  
@@ -29,17 +58,16 @@ while(True):
 
         else:
             term_print('FingerPrint Incorrect')
-            wait(1)
+            wait(1)                           #instead of wait use the time diff
             flag=0
             continue
 
     elif(flag==2):
-        term_print('FingerPrint?')
-        wait(0.5)
-
-    elif(flag==3):
         rfidToServer(inp)
         flag=0
+    elif(flag==3 or flag==4 or flag==):
+        term_print('FingerPrint?')
+        wait(0.5)
 
     else:
         screensaver()
